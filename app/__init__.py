d46b37257d2d07f41c5cf82bc8566fbd057725d3
Riccardo <riccardo.masetti@outlook.com>
2025-05-10 23:53:43 +0200

    config model file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 
 def create_app():
-
+    load_dotenv()
     app = Flask(__name__)
 
     @app.route('/')
